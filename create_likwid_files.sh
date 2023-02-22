f59932c8bb8026657daf4f09f6752642c08fa1ea
gabdepa <gabriel-r-p-p@hotmail.com>
2023-02-21 21:45:29 -0300

    Adjustments on .sh

---
+++ create_likwid_files.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+METRICA="L3 L2CACHE FLOPS_DP FLOPS_AVX" # Metrics for Likwid Markers
+
+SIZE="32 64 128 256 512 1000" # Size of matrix nxn where SIZE=n
+
+if [ -f *.csv ]; then
+    rm *.csv && echo -e ".csv files cleaned" # Cleaning previous log files
+fi
+
+echo -e "\n> Make purge:" && make purge
+
+echo -e "\n> Compiling..." && make
+
+echo -e "\n> Running FIRST version, cgsolver_v1\n:"
+
+echo "Compilation Result:"
+for k in $METRICA; do
+     for s in $SIZE; do
+         likwid-perfctr -O -C 3 -g ${k} -m ./cgSolver_v1 -n $s -i 150 -k 7 -p 1 -o saida_v1.txt -d diagonal > ${s}_${k}_v1.csv
+         echo -e "=> File ${k}_${s}_v1.csv Created Successfully."
+     done
+done
+
+# sed -i '7 s/CFLAGS=-Wall -I${LIKWID_INCLUDE} -DLIKWID_PERFMON/CFLAGS=-Wall -I${LIKWID_INCLUDE} -DLIKWID_PERFMON -O3 -mavx2 -march=native/' makefile # Modify CFLAGS in makefile to add otimization flags
+
+echo -e "\n>Running SECOND version, cgsolver_v2\n:"
+
+echo "Compilation Result:"
+for k in $METRICA; do
+    for s in $SIZE; do
+        likwid-perfctr -O -C 3 -g ${k} -m ./cgsolver_v2 -n $s -i 150 -k 7 -p 1 -o saida_v2.txt -d diagonal > ${s}_${k}_v2.csv
+        echo -e "=> File ${s}_${k}_v2.csv Created Successfully."
+    done
+done
+echo -e "\nDone" # Script finished
+
+# echo -e "\nCalling create_csv_files.py...\n"
+# python3 create_csv_files.py
